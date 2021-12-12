d630688c4da9f287281c9cc0829d94f7eac41940
Sebastian Orduz <sebastianorduz97@gmail.com>
2021-12-12 08:52:50 -0500

    document generic_model_mutation_process

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,21 @@
-def generic_model_mutation_process(model, data, id=None, commit=True):
+from django.db.models import Model
+
+
+def generic_model_mutation_process(
+    model: Model,
+    data: dict,
+    id: int = None,
+    commit: bool = True,
+) -> Model:
+    """
+    Updates or creates Django model object.
+
+    :param Model model: Django model class
+    :param dict data: Model object data
+    :param int id: Object id
+    :param bool commit: Save model default True
+    :return: django model instance
+    """
     if id:
         item = model.objects.get(id=id)
         try:
